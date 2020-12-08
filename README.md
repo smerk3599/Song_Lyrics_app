887d132d26efe390cdda445d084eea1198411c54
Steven Mercer <stevenmercer9913@gmail.com>
2020-12-08 13:52:11 -0800

    some responsive design, readMe update, comments and cleanup

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Song_Lyrics_app
 User requested song lyrics site
+
+This site uses an API to get the lyrics of a song based on user input.    The apiI used was lyrics.ovh.
+The user input would be in the form of a singer or band's name and then the name of the song.
+The API uses both names together in one call to get the lyrics.
+
+Since I needed two different names, I used a modal to separate the input, and then a second modal to display the lyrics.  There are buttons on each modal to return to the previous modal.
+
+I really wanted to set it up to either change the background picture every so often or once I new the singer/musicians name to show random pictures from that artist. Ah, ambitions.   However, I really liked how the background picture I used set up with everything.  Sometimes less is more.
+
+Issues.
+- Initially, the format of the lyrics was really difficult to deal with.    I tried to process the lyrics as a string, using the carriage return character to separate each line into its' own div because it would format nicely in the console.log, but not so much on the screen.   Putting everything into a single div did not format it correctly, it just all came out in a paragraph, and until I found an element called textarea the string processing would just randomly stop way before it was finished.  When I put the whole lyrics off the API inside the textarea, it was magically formatted.   I almost had to change APIs because of this issue and how long it took me to figure out.
+
+- The modals took a little bit of playing around with to figure out how to do them this way, but overall it went fairly smoothly.
+
+- The text inputs were a little challenging until I found the autofocus and focus properties, and then things worked just fine.
+
+- One little bit of weirdness that I never solved so I took out, was the title on the second modal.  I wanted it to include the band name and song name, but the song name never would show up either in a console log or on the page.  Being similarly defined, I thought bandName and songName would behave the same way on the page, but songName had a mind of it's own.  It looks like all of that info would have been too long on one line anyway for some songs.
+
+- The API seems a little unreliable, but only 1-2% of the time.
--- app.js
+++ app.js
@@ -1,63 +1,6 @@
 $(() => {
 
 
-
-
-  //   const testString = `Relax don't do it
-  //   ↵When you want to go to it
-  //   ↵Re`;
-  // //The function to process the lyrics into 1 line divs that are readable
-  // const processStringOfLyrics = (object) => {
-  //
-  //   //Initialize a local array to hold each letter of the string
-  //   const stringArray =[];
-  //   // console.log(object);
-  //   let string = object.lyrics;
-  //   // console.log(string.length);
-  //   //For loop to break the lyric string into an array
-  //   for (var i = 0; i < string.length; i++) {
-  //     if ((/[ -z]/).test(string[i]) || (string[i] === '\r')){
-  //       stringArray[i] = string[i];
-  //     }
-  //   }
-  //   console.log(stringArray);
-  //
-  //   //***    object.lyrics.replace(/[\r]/g, '<br>');
-  //   //*** $('.lyricsBox').text(object.lyrics);
-  //   //Create the first div that the first line of the lyrics will hold
-  //   let $eachLyricDiv = $('<div class="lyricLine">');
-  //   //Initialize a variable for the string of each line in the lyrics
-  //   let eachLine = '';
-  //
-  //   //A for loop to examine each letter of the lyrics
-  //   for (each of stringArray) {
-  //     //This API is set up using a carriage return charcter to seperate each line in the lyrics
-  //     // If the letter is not a carriage return character
-  //     // console.log(each);
-  //     // if (each !== ''){
-  //
-  //       if (each !== '\r'){
-  //         //then add each letter to the string
-  //         eachLine += `${each}`;
-  //         // console.log(eachLine);
-  //         // If it is a carriage return, start a new line of lyrics
-  //       } else {
-  //       // const lyricsWithLineBreaks = object.lyrics.replace(/\u21B5/g, '<br/>');
-  //         //Make the text of a div the string of one line of lyric
-  //         console.log(eachLine);
-  //         console.log('In the else');
-  //         // eachLine += `<br>`;
-  //         $eachLyricDiv.text(eachLine);
-  //         //Append that div to the main div for the box the lyrics will be in
-  //         $eachLyricDiv.appendTo('.lyricsBox');
-  //         //Create a new div for the next line in the song
-  //         $eachLyricDiv = $('<div class="lyricLine">');
-  //         //Reset the string of each line in the lyrics
-  //         eachLine = '';
-  //       }
-  //     // }
-  //   }
-  // }
   //Initialize the BandName and SongName variables
   let bandName = '';
   let songName = '';
@@ -76,33 +19,50 @@ $(() => {
 
   //Function to open modal 1
   const openModal1 = () => {
+    //Make the title of the first modal the name of the singer/musician
     $('#page-2-title').text(`${bandName}`);
+    //Show modal 1
     $modal1.css('display', 'block');
+    //Hide the first page
     $openButton1.css('display', 'none');
-    $('#band').trigger('reset');
+    // Make the cursor appear in the song title input box
     $('#songName').focus();
   }
 
   //Function to open modal 2
   const openModal2 = () => {
+    //originally a title for the second modal that included
+    // $('#page-3-title').text(`Song Lyrics for ${bandName}'s  ${songName}`);
+    //hide modal 1
     $modal1.css('display', 'none');
+    //Show modal 2
     $modal2.css('display', 'block');
-    $('#songName').trigger('reset');
   }
 
   //Function to close modal 1
   const closeModal1 = () => {
+    //hide modal 1
     $modal1.css('display', 'none');
+    //Show the first page
     $openButton1.css('display', 'block');
+    // Make the cursor appear in the song title input box
     $('#band').focus();
-    $('#songName').trigger('reset');
+    //reset the band name variable when the first modal closes
+    bandName = '';
   }
 
   //Function to close modal 2
   const closeModal2 = () => {
+    //hide modal 2
     $modal2.css('display', 'none');
+    //Show modal 1
     $modal1.css('display', 'block');
+    // Make the cursor appear in the song title input box
     $('#songName').focus();
+    //reset the text area when the second modal closes
+    $('#lyricsBox').text('');
+    //reset the song name variable when the second modal closes
+    songName = '';
   }
 
   //Event listeners to open each modal
@@ -120,7 +80,7 @@ $(() => {
     // Assign a variable to the name of the band
     bandName = $('#band').val();
     console.log(bandName);
-
+    //reset the musician input box
     $('#musician').trigger('reset');
   });
 
@@ -129,9 +89,9 @@ $(() => {
     event.preventDefault();
     // Assign a variable to the name of the song
     songName = $('#songName').val();
-    console.log(songName);
     console.log(`${bandName}/${songName}`);
-    $('getSong').trigger('reset');
+    //reset the song input box
+    $('#getSong').trigger('reset');
 
     // Ajax call for the API
     $.ajax({
@@ -140,15 +100,13 @@ $(() => {
     }).then(
 
       (song)=>{
-        console.log(song.lyrics);
+        // assign the retreived lyrics to the text area
         $('#lyricsBox').text(song.lyrics);
-      // processStringOfLyrics(song);
-      // let $eachLyricDiv = $('<div class="lyricLine">');
-      // $eachLyricDiv.text(song.lyrics);
-      // //Append that div to the main div for the box the lyrics will be in
-      // $eachLyricDiv.appendTo('.lyricsBox');
+        console.log(songName);
+
       },
       ()=>{
+        //if nothing comes back from the api send an alert to the user
         console.log('bad request');
         alert("What you entered doesn't return a song!");
       }
